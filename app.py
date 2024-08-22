c2fb0bb7cb0a4faa7b18bf6d806398b536f768e3
Febrian Alexandro <105537730+AlexandroFebrian@users.noreply.github.com>
2024-08-22 14:19:19 +0700

    push

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask_cors import CORS
 import joblib
 import pandas as pd
 import re
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 CORS(app)
 
-model = joblib.load('XGBoost_model_8depth_50.pkl')
+model = load_model('NN_model_50_inverse.h5')
 encoder = joblib.load('encoder.pkl')
 
 @app.route('/')
@@ -31,58 +32,27 @@ def predict():
   if file.filename == '':
     return jsonify({'error': 'No file selected for uploading'}), 400
   
-  if file and file.filename.endswith('.xlsx'):
-    new_data = pd.read_excel(file, skiprows=6)
+  if file and file.filename.endswith('.csv'):
+    data = pd.read_csv(file, delimiter=',|;')
 
-    cabang = pd.read_excel(file, skiprows=1).iloc[0, 2]
-    cabang = cabang.split()[0]
-    
-    new_data = new_data[['CONSIGNEE', 'CONTAINER', 'CARGO']]
-    new_data['CABANG'] = cabang
+    try:
+      data = data[['CONSIGNEE', 'SIZE', 'CARGO', 'CABANG', 'GRADE']]
+    except:
+      data = data[['CONSIGNEE', 'SIZE', 'CARGO', 'CABANG', 'CONT. GRADE']]
+      data = data.rename(columns={'CONT. GRADE': 'GRADE'})
+      
 
-    new_data['CONTAINER'] = new_data['CONTAINER'].str.replace('SPNU', '')
-    new_data['CONTAINER'] = new_data['CONTAINER'].str[:3]
-    new_data['CONTAINER'] = pd.to_numeric(new_data['CONTAINER'], errors='coerce')
-    new_data['GRADE'] = new_data['CONTAINER'].apply(assign_grade)
-    new_data['SIZE'] = new_data['CONTAINER'].apply(assign_size)
-    new_data = new_data.drop(['CONTAINER'], axis=1)
-
-    
-    encoded_cols = encoder.transform(new_data[['CONSIGNEE', 'SIZE', 'CARGO', 'CABANG', 'GRADE']])
+    encoded_cols = encoder.transform(data[['CONSIGNEE', 'SIZE', 'CARGO', 'CABANG', 'GRADE']])
     encoded_data_test = pd.DataFrame(encoded_cols, columns=encoder.get_feature_names_out(['CONSIGNEE', 'SIZE', 'CARGO', 'CABANG', 'GRADE']))
-    new_data_encoded = pd.concat([new_data.reset_index(drop=True), encoded_data_test], axis=1)
-    new_data_encoded = new_data_encoded.drop(['CONSIGNEE', 'SIZE', 'CARGO', 'CABANG', 'GRADE'], axis=1)
 
-    regex = re.compile(r"\[|\]|<", re.IGNORECASE)
-    new_data_encoded.columns = [regex.sub("_", col) if any(x in str(col) for x in set(('[', ']', '<'))) else col for col in new_data_encoded.columns.values]
+    df_encoded = pd.concat([data.reset_index(drop=True), encoded_data_test], axis=1)
+    df_encoded = df_encoded.drop(['CONSIGNEE', 'SIZE', 'CARGO', 'CABANG', 'GRADE'], axis=1).values
     
-    predictions = model.predict(new_data_encoded)
+    predictions = model.predict(df_encoded)
     
     return jsonify({'predictions': predictions.tolist()})
   else:
-    return jsonify({'error': 'Invalid file format, only .xlsx allowed'}), 400
-    
-def assign_grade(container):
-  if 290 <= container <= 400:
-    return 'A'
-  elif 280 <= container <= 289:
-    return 'B'
-  elif container <= 279:
-    return 'C'
-  elif container >= 463:
-    return 'A'
-  elif 461 <= container <= 462:
-    return 'B'
-  elif container == 460:
-    return 'C'
-  else:
-    return 'Unknown'
-
-def assign_size(container):
-  if container <= 400:
-    return 20
-  else:
-    return 40
+    return jsonify({'error': 'Invalid file format, only .csv allowed'}), 400
 
 if __name__ == '__main__':
     app.run(
